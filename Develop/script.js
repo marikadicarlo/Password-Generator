50469c9db297d289948ff518c5b25d8f176bc5d0
marikadicarlo <mdicarlo19@yahoo.com>
2021-12-09 22:21:01 -0500

    updated to js

--- Develop/script.js
+++ Develop/script.js
@@ -1,64 +1,97 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberChar = "0123456789";
-var specialChar = "~`!@#$%^&*()_+-={}[]\:;'<>?,./|";
-var passwordLength;
-var uppercase;
-var lowercase;
-var numbers;
-var specialChars;
 
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number = "0123456789";
+var special = "~`!@#$%^&*()_+-={}[]\:;'<>?,./|";
 
-// Create function for Generate Password
-function generatePassword(){
-  console.log('User clicked button');
+var availableChar = ""
 
-// Prompt user for password criteria
-var promptPass = window.prompt('How many CHARACTERS would you like your password to inlcude?');
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  if (passwordLength < 8){
-    window.alert('Password must be a number between 8 - 128 characters!');
-    determineLength();
-    return promptPass();
-  }
-  else if (passwordLength > 128){
-    window.alert('Password must be a number between 8 - 128 characters!');
-    determineLength();
-    return promptPass();
-  }
-  else if (promptPass === "" || promptPass === null) {
-    window.alert('Password must be a number between 8 - 128 characters. Try again!');
-    return generatePassword();
-  }
+  passwordText.value = password;
+
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
-// Create function for user to include uppercase characters
-//var promptPass = window.confirm('Select OK to confirm using special characters.');
+// var password = "";
+// var passwordChar = "";
 
+// Create function for Generate Password
+function generatePassword() {
+availableChar = ""
+  console.log('test');
 
-// Alert to confirm inlcuding numeric characters
-//var promptPass = window.confirm('Select OK to confirm using numeric characters.');
+// Prompt user for password criteria
+var passwordPrompt = window.prompt('How many CHARACTERS would you like your password to inlcude?');
+passwordPrompt = parseInt(passwordPrompt);
+console.log('test');
 
+  if (passwordPrompt < 8){
+    window.alert('Password must be a number between 8 - 128 characters!');
+    return "";
+  }
+  else if (passwordPrompt > 128){
+    window.alert('Password must be a number between 8 - 128 characters!');
+    return "";
+  }
+  else if (passwordPrompt === "" || passwordPrompt === null) {
+    window.alert('Password must be a number between 8 - 128 characters. Try again!');
+    return "";
+  }
+  else if (isNaN (passwordPrompt)) {
+    window.alert('Password must be a unmber between 8 -128 characters. Try again!');
+  }
 
-// Alert to confirm inlcuding lowercase characters
-//var promptPass = window.confirm('Select OK to confirm using lowercase characters.');
+  var specialChar = window.confirm('Select OK to confirm using special characters.');
+  var numericChar = window.confirm('Select OK to confirm using numeric characters.');
+  var lowercaseChar = window.confirm('Select OK to confirm using lowercase characters.');
+  var uppercaseChar =  window.confirm('Select OK to confirm using uppercase characters.');
+  console.log(specialChar, numericChar, lowercaseChar, uppercaseChar);
 
+  var finalPassword = [];
+  var counter = 0;
 
-// Alert to confirm including uppercase characters
-//var promptPass =  window.confirm('Select OK to confirm using uppercase characters.');
+  if (specialChar) {
+    availableChar = availableChar + special;
+    var random = Math.floor(Math.random() * special.length);
+    finalPassword.push(special.charAt(random))
+    counter++
+  }
+  if (numericChar) {
+    availableChar = availableChar + number;
+    var random = Math.floor(Math.random() * number.length);
+    finalPassword.push(number.charAt(random))
+    counter++
+  }
+  if (lowercaseChar) {
+    availableChar = availableChar + lowercase;
+    var random = Math.floor(Math.random() * lowercase.length);
+    finalPassword.push(lowercase.charAt(random))
+    counter++
+  }
+  if (uppercaseChar) {
+    availableChar = availableChar + uppercase;
+    var random = Math.floor(Math.random() * uppercase.length);
+    finalPassword.push(uppercase.charAt(random))
+    counter++
+  }
+  console.log('available characters', availableChar);
+  
 
-
-
-// Validate password criteria
-
-// Generate password based on criteria
-
-// Display password to the page
-
-
+  for (var i = 0; i < passwordPrompt - counter; i++) {
+    var random = Math.floor(Math.random() * availableChar.length);
+    console.log(random);
+    finalPassword.push(availableChar.charAt(random));
+  }
+  console.log(finalPassword);
+  return finalPassword.join("");
+}
 
 
 // Write password to the #password input
